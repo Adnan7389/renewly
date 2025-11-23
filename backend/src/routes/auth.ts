6803e435b35d35bac929b301472b1198e5d4e0ac
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-11-23 23:29:24 +0300

    Convert src/routes/auth.js to TypeScript

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, refresh, logout } from '../controllers/authController.js';
+import { register, login, refresh, logout } from '../controllers/authController';
 
 const router = express.Router();
 
